13ebd483c299cd58932ff709558c74d86a8a517c
Fidéle <yonlifidelis2@gmail.com>
2022-03-23 11:54:28 +0100

    yonli

---
+++ app/src/main/java/com/example/elearning/SousCategory.java
@@ -0,0 +1,13 @@
+package com.example.elearning;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SousCategory extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.souscategory);
+}
+}
